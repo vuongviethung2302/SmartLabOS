ab65a1c5ff43f20879a23594db60459f19038a0f
Vuong Viet Hung <vuongviethung2302@gmail.com>
2026-07-21 20:18:53 +0700

    Setup virtual environment and simplify project dependencies

--- test_scanner.py
+++ test_scanner.py
@@ -2,6 +2,6 @@ from services.scanner.network_scanner import NetworkScanner
 
 scanner = NetworkScanner()
 
-online = scanner.scan_subnet("10.170.44.0/24")
+online = scanner.scan_subnet("127.0.0.0/30")
 
 print(online)
